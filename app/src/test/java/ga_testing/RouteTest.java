d58ce0756fe20fe168e5b8f44fb12a1e53970602
mm3176 <93787471+mm3176@users.noreply.github.com>
2023-08-31 11:07:32 +0100

    Added test placeholders to RouteTest to know what to test for

---
+++ app/src/test/java/ga_testing/RouteTest.java
@@ -0,0 +1,32 @@
+package ga_testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ga_ts.City;
+import ga_ts.Route;
+
+public class RouteTest {
+
+    @Test
+    void testRouteCreation(){
+
+    }
+
+    @Test
+    void testRouteDistance(){
+
+    }
+
+    @Test
+    void testRouteGoodFitness(){
+
+    }
+
+    @Test
+    void testRouteBadFitness(){
+
+    }
+
+}
