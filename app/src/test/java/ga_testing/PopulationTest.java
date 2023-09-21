df20cddf81b57093a76772be2eec01820ca642e3
mm3176 <93787471+mm3176@users.noreply.github.com>
2023-09-21 11:37:52 +0100

    Added test to test whether a Population is being created.

---
+++ app/src/test/java/ga_testing/PopulationTest.java
@@ -0,0 +1,58 @@
+package ga_testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ga_ts.City;
+import ga_ts.Population;
+
+import java.util.ArrayList;
+
+public class PopulationTest {
+
+    @Test
+    void testCreatePopulation(){
+        //Creating our list of cities
+        City city1 = new City(2, 3);
+        City city2 = new City(6, 6);
+        City city3 = new City(13, 9);
+        City city4 = new City(20, 12);
+        City city5 = new City(8, 1);
+        City city6 = new City(15, 10);
+        City city7 = new City(5, 11);
+        City city8 = new City(10, 19);
+        City city9 = new City(18, 7);
+        ArrayList<City> cities = new ArrayList<>();
+        cities.add(city1);
+        cities.add(city2);
+        cities.add(city3);
+        cities.add(city4);
+        cities.add(city5);
+        cities.add(city6);
+        cities.add(city7);
+        cities.add(city8);
+        cities.add(city9);
+
+        //Creating our population
+        Population pop = new Population(cities);
+        int targetSize = 20;
+
+        //checking the length of the population
+        assertEquals(targetSize, pop.routes.size());
+
+    }
+
+    @Test
+    void testAverageFitness(){
+
+    }
+
+    @Test
+    void testHighestFitness(){
+
+    }
+
+
+
+}
