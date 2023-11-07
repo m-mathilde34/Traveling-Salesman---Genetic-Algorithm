7fa7df97e6fb7f9f6c33ee10a4e9b399c5094b90
mm3176 <93787471+mm3176@users.noreply.github.com>
2023-11-07 12:41:37 +0000

    Added skeleton of methods for our CityGenerator. Added comments.

--- app/src/main/java/ga_ts/CityGenerator.java
+++ app/src/main/java/ga_ts/CityGenerator.java
@@ -1,4 +1,52 @@
 package ga_ts;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class CityGenerator {
+
+    public int cityNumber;
+    public int minValue;
+    public int maxValue;
+    public double minDistanceRadius;
+    public ArrayList<City> generatedCities;
+
+    /**
+     * Constructor method which creates a list of cityNumber of cities, with a minimum distance between them.
+     * These cities are then written onto a file.
+     * @param cityNumber, the number of city to be created by our generator.
+     * @param minValue, the minimum value for our x and y values.
+     * @param maxValue, the maximum value for our x and y values.
+     * @param minDistanceRadius, the minimum distance between each city.
+     */
+    public CityGenerator(int cityNumber, int minValue, int maxValue, double minDistanceRadius){
+        this.cityNumber = cityNumber;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.minDistanceRadius = minDistanceRadius;
+        generatedCities = generateListOfCities();
+    }
+
+    /**
+     * Creates a random list of cityNumber cities. Each city's x and y value is set between minValue and maxValue.
+     * Each city has to be positioned with a minimum distance between previously created cities, following the
+     * minDistanceRadius.
+     * @return generatedCities, an ArrayList of cities.
+     *
+     */
+    public ArrayList<City> generateListOfCities(){
+        ArrayList<City> generatedCities = new ArrayList<>();
+
+        return generatedCities;
+    }
+
+    /**
+     * Randomly sets a x and y value in order to create a city.
+     * @return city, a City with a position of (x,y)
+     *
+     */
+    public City generateNewCity(){
+
+        return city;
+    }
 }
