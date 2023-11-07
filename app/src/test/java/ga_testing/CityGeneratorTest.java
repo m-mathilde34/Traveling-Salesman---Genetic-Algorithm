f2dd4f6230fb350fff81374c26514a102cf520e3
mm3176 <93787471+mm3176@users.noreply.github.com>
2023-11-07 15:14:47 +0000

    Added CityGeneratorTest test skeletons

--- app/src/test/java/ga_testing/CityGeneratorTest.java
+++ app/src/test/java/ga_testing/CityGeneratorTest.java
@@ -1,4 +1,22 @@
 package ga_testing;
 
+import org.junit.jupiter.api.Test;
+
 public class CityGeneratorTest {
+
+    @Test
+    void testCityGeneratorSmallSample(){
+
+    }
+
+    @Test
+    void testCityGeneratorMediumSample(){
+
+    }
+
+    @Test
+    void testCityGeneratorLargeSample(){
+
+    }
+
 }
