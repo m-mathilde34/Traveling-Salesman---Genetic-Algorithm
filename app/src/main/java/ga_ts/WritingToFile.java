a4d49871c682dbb34da9a1d56e7bab28307a527a
mm3176 <93787471+mm3176@users.noreply.github.com>
2023-12-05 17:00:02 +0000

    Added implementation to WritingToFile class and its Test class.

--- app/src/main/java/ga_ts/WritingToFile.java
+++ app/src/main/java/ga_ts/WritingToFile.java
@@ -1,4 +1,47 @@
 package ga_ts;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class WritingToFile {
+
+    public String filename;
+
+    public WritingToFile(String filename){
+        this.filename = filename;
+    }
+
+    public void saveToCSV(int generations, ArrayList<Double> averageFitnessPerGen, ArrayList<Double> bestFitnessPerGen,
+                          int popSize, int poolSize, float mutationRate){
+        File csvFile = new File(this.filename);
+        try(FileWriter fileCSVWriter = new FileWriter(csvFile)) {
+
+            //Create a header
+            fileCSVWriter.write("Generation, Average Fitness, Best Fitness, Population Size, Pool Size, Mutation Rate");
+            fileCSVWriter.write(System.getProperty("line.separator"));
+
+            int counter=0;
+            while(counter<generations){
+                //Generation, PopSize, PoolSize, MutationRate,
+                fileCSVWriter.write(Integer.toString(counter+1) + "," +
+                        Double.toString(averageFitnessPerGen.get(counter)) + "," +
+                        Double.toString(bestFitnessPerGen.get(counter)) + "," +
+                        Integer.toString(popSize) + "," +
+                        Integer.toString(poolSize) + "," +
+                        Float.toString(mutationRate));
+                //Next line
+                fileCSVWriter.write(System.getProperty("line.separator"));
+
+                counter++;
+            }
+
+            fileCSVWriter.close();
+        } catch(IOException e){
+            System.out.println("Error Occurred Writing To File! " + e.getMessage());
+        }
+    }
+
+
 }
--- app/src/test/java/ga_testing/WritingToFileTest.java
+++ app/src/test/java/ga_testing/WritingToFileTest.java
@@ -1,4 +1,31 @@
 package ga_testing;
 
+import ga_ts.WritingToFile;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 public class WritingToFileTest {
+
+    @Test
+    void testWritingToFile(){
+        int gen = 3;
+        int popSize = 10;
+        int poolSize = 4;
+        float mutationRate = 0.10f;
+        ArrayList<Double> averageFitnessPerGen = new ArrayList<>();
+        ArrayList<Double> bestFitnessPerGen = new ArrayList<>();
+
+        averageFitnessPerGen.add(1.273924);
+        averageFitnessPerGen.add(1.928393);
+        averageFitnessPerGen.add(1.354893);
+
+        bestFitnessPerGen.add(1.19293);
+        bestFitnessPerGen.add(1.29393);
+        bestFitnessPerGen.add(1.23839);
+
+        WritingToFile writingToFile = new WritingToFile("Test.csv");
+        writingToFile.saveToCSV(gen, averageFitnessPerGen, bestFitnessPerGen, popSize, poolSize, mutationRate);
+
+    }
 }
